e786b1c74e8382c931bd3f57c9ebb7f4fa981793
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2006-09-04 12:21:37 +0000

    fixed dynamicArray bugs (also in InputStreams)

--- src/mcast/object/Inputstreams.java
+++ src/mcast/object/Inputstreams.java
@@ -3,82 +3,91 @@ package mcast.object;
 public class Inputstreams {
 
     private static final int DEFAULT_SIZE = 64;
-    
-    private LRMCInputStream [] inputStreams = new LRMCInputStream[DEFAULT_SIZE];    
-    private boolean [] hasData = new boolean[DEFAULT_SIZE];
-    
+
+    private LRMCInputStream[] inputStreams = new LRMCInputStream[DEFAULT_SIZE];
+
+    private boolean[] hasData = new boolean[DEFAULT_SIZE];
+
     private int streamsWithData = 0;
+
     private int index = 0;
-            
+
+    private int last = -1;
+    
     public synchronized void add(LRMCInputStream is, short sender) {
-        
-     //   System.err.println("Added sender " + sender);
-        
-        if (sender >= inputStreams.length) {             
+        System.err.println("Added sender " + sender);
+
+        if (sender >= inputStreams.length) {
             resize(sender);
         }
-        
-        inputStreams[sender] = is;        
-    } 
-    
-    private void resize(int minimumSize) { 
-        
-        int newSize = hasData.length;        
-        
-        while (newSize <= minimumSize) {
-            newSize *= 2; 
+
+        inputStreams[sender] = is;
+        if (sender > last) {
+            last = sender;
         }
-        
-        LRMCInputStream [] tmp1 = new LRMCInputStream[newSize];
+    }
+
+    private void resize(int minimumSize) {
+        int newSize = hasData.length;
+
+        while (newSize <= minimumSize) {
+            newSize *= 2;
+        }
+
+        LRMCInputStream[] tmp1 = new LRMCInputStream[newSize];
         System.arraycopy(inputStreams, 0, tmp1, 0, inputStreams.length);
         inputStreams = tmp1;
-                        
-        boolean [] tmp2 = new boolean[newSize];        
+
+        boolean[] tmp2 = new boolean[newSize];
         System.arraycopy(hasData, 0, tmp2, 0, hasData.length);
         hasData = tmp2;
     }
+
+    public synchronized LRMCInputStream find(short sender) {
+        if(sender < 0 || sender > last) {
+            System.err.println("Illegal find in InputStreams");
+            return null;
+        }
         
-    public synchronized LRMCInputStream find(short sender) {         
-        return inputStreams[sender];        
+        return inputStreams[sender];
     }
-    
-    public void returnStream(LRMCInputStream is) {        
+
+    public void returnStream(LRMCInputStream is) {
         if (is.haveData()) {
             hasData(is);
-        }        
-    }    
-    
-    public synchronized void hasData(LRMCInputStream is) {        
-        hasData[is.getSource()] = true;        
-        streamsWithData++;
-        
-        if (streamsWithData > 1) {                 
-            notifyAll();
-        } 
+        }
     }
-    
-    public synchronized LRMCInputStream getNextFilledStream() {
 
-        while (streamsWithData == 0) { 
-            try { 
+    public synchronized void hasData(LRMCInputStream is) {
+        hasData[is.getSource()] = true;
+        streamsWithData++;
+
+        if (streamsWithData > 1) {
+            notifyAll();
+        }
+    }
+
+    public synchronized LRMCInputStream getNextFilledStream() {
+        while (streamsWithData == 0) {
+            try {
                 wait();
             } catch (Exception e) {
                 // ignore
-            }            
+            }
         }
-        
+
         int size = inputStreams.length;
-                
-        for (int i=0;i<size;i++) { 
-            
-            if (hasData[(index+i)%size]) { 
-                index = (index+i)%size;
+
+        for (int i = 0; i < size; i++) {
+
+            if (hasData[(index + i) % size]) {
+                index = (index + i) % size;
                 hasData[index] = false;
             }
         }
 
         streamsWithData--;
-        
+
         return inputStreams[index];
     }
 }
--- src/mcast/util/DynamicObjectArray.java
+++ src/mcast/util/DynamicObjectArray.java
@@ -2,69 +2,69 @@ package mcast.util;
 
 public class DynamicObjectArray {
 
-    private static final int DEFAULT_SIZE = 64;   
-    private Object [] objects;
+    private static final int DEFAULT_SIZE = 64;
+
+    private Object[] objects;
+
     private int last = -1;
-    
-    public DynamicObjectArray() { 
+
+    public DynamicObjectArray() {
         this(DEFAULT_SIZE);
     }
-    
-    public DynamicObjectArray(int size) {         
-        objects = new Object[size]; 
-    } 
-    
-    private void resize(int minimumSize) { 
-        
-        int newSize = objects.length;        
-        
+
+    public DynamicObjectArray(int size) {
+        objects = new Object[size];
+    }
+
+    private void resize(int minimumSize) {
+        int newSize = objects.length;
+
         while (newSize <= minimumSize) {
-            newSize *= 2; 
+            newSize *= 2;
         }
-        
-        Object [] tmp = new Object[newSize];
+
+        Object[] tmp = new Object[newSize];
         System.arraycopy(objects, 0, tmp, 0, objects.length);
         objects = tmp;
     }
-    
-    public void put(int index, Object o) { 
-     
-        if (index >= objects.length) { 
+
+    public void put(int index, Object o) {
+        if (index >= objects.length) {
             resize(index);
         }
-        
+
         objects[index] = o;
-        
-        if (index > last) { 
+
+        if (index > last) {
             last = index;
-        }        
-    }
-    
-    public void remove(int index) {         
-        if (index < objects.length) { 
-            objects[index] = null;
-            
-            if (index == last) {                               
-                while (index >= 0 && objects[index] == null) {                    
-                    index--;
-                }
-                             
-                last = index;                
-            }            
         }
     }
-    
-    
-    public Object get(int index) { 
-        
-        if (index < 0 || index >= objects.length) { 
+
+    public void remove(int index) {
+        if (index > last) {
+            System.err.println("illegal remove in DynamicObjectArray");
+            return;
+        }
+
+        objects[index] = null;
+        if (index == last) {
+            while (index >= 0 && objects[index] == null) {
+                index--;
+            }
+            last = index;
+        }
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index > last) { 
+            System.err.println("illegal get in DynamicObjectArray");
             return null;
         }
-        
-        return objects[index];        
-    }    
-    
-    public int last() { 
+
+        return objects[index];
+    }
+
+    public int last() {
         return last;
-    }   
+    }
 }
